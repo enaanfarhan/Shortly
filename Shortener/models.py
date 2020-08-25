30eff4fd1b2b3353ba8d5467536c885b8d60fa75
enaanfarhan <enaanfo@gmail.com>
2020-08-26 01:19:52 +0600

    Third commit

--- Shortener/models.py
+++ Shortener/models.py
@@ -1,9 +1,18 @@
 from django.db import models
-
+from .views import *
 # Create your models here.
+
+
 class shortlyURL(models.Model):
     url = models.CharField(max_length=220, )
-    shortcode = models.CharField(max_length=15, unique=True)
+    shortcode = models.CharField(max_length=15, unique=True, blank=True)
+    update = models.DateField(auto_now=True)
+    timestamp = models.DateField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.shortcode is None or self.shortcode == "":
+            self.shortcode = code_generator()
+        super(shortlyURL, self).save(*args, **kwargs)
 
     def __str__(self):
         return str(self.url)
--- Shortener/views.py
+++ Shortener/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+import random
+import string
 
 # Create your views here.
+def code_generator(size=6, char=string.ascii_lowercase + string.digits + string.ascii_uppercase):
+    return ''.join(random.choice(char) for i in range(size))
